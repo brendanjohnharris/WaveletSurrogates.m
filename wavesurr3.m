66f35b1bc6709b8953c17d73ab1c82f687a53771
brendanjohnharris <brendanjohnharris@gmail.com>
2022-06-15 20:17:59 +1000

    Comment

--- wavesurr3.m
+++ wavesurr3.m
@@ -1,10 +1,8 @@
-function wavesurr3(X, options)
+function wavesurr3(X)
 % WAVESURR3 Surrogate data for a two-dimensional time series via the wavelet transform
 
 arguments
     X (:,:,:) {mustBeNumeric}
-    options.wavelet = 'sym4'; % See https://au.mathworks.com/help/wavelet/ug/wavelet-analysis-for-3d-data.html
-    options.levels int64 = wmaxlev(min(size(X)), options.wavelet)
 end
 
 if mod(size(X, 1), 2); X = X(1:end-1, :, :); end
@@ -14,7 +12,7 @@ mu = mean(X, 'all', 'omitnan');
 sigma = std(X, 0, 'all', 'omitnan');
 X = X - mu;
 nanidxs = isnan(X);
-X(nanidxs) = 0; % ! Will have to check the validity of this
+X(nanidxs) = 0; % ! Will have to check the validity of this. Should be fine, since wavelet method won't 'mix in the edge' like the Fourier approach, since it is designed from non-stationarity. At least for the small wavelets; might be a problem for the very largest wavelets though, but those are spatially filtered anyway...
 
 % 1. generate a Gaussian white noise time series to match the original data length
 X_ = randn(size(X));
@@ -31,8 +29,7 @@ X_ = idualtree3(a, d);
 X_ = sigma.*X_./std(X_, 0, 'all', 'omitnan');
 
 % 5. rescale the surrogate x^(t) to the distribution of the original time series by sorting the data (after a wavelet filtering in the frequency band of interest) according to the ranking of values of the wavelet-based surrogate11,
-% ! Won't do this, since sorting data over multiple dimensions is not clear. If you like you can try:
-
+% ! Won't do this for now, since sorting data over multiple dimensions is not clear.
 
 X_ = X_ + mu;
 X_(nanidxs) = NaN
